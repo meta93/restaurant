51b405aae94d38f115bf1989c807abe6d884a162
meta93 <bismarkkyeremateng93@gmail.com>
2021-06-11 02:37:05 +0000

    k

---
+++ components/TopComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TopComponent() {
+    return (
+       <>
+            <div className='background'></div>
+            <div className='className'>
+                <Container className="container-top"></Container>
+            </div>
+       </>
+    )
+}
+
+export default TopComponent
